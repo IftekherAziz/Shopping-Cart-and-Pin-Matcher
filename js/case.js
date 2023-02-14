b43eaa7f7e1f5efc24197607efff9764991862d4
Iftekher Aziz <azizchy02@gmail.com>
2023-02-15 01:58:00 +0600

    Update Case and Phone JS file

--- js/case.js
+++ js/case.js
@@ -1,1 +1,39 @@
-console.log('case.js loaded');
+/* Common function to update case price according to plus and minus */
+
+function updateCaseNumber(isIncrease) {
+  const caseNumberField = document.getElementById("case-number-field");
+  const caseNumberString = caseNumberField.value;
+  const previousCaseNumber = parseInt(caseNumberString);
+  let newCaseNumber;
+
+  if (isIncrease === true) {
+    newCaseNumber = previousCaseNumber + 1;
+  } else {
+    if (previousCaseNumber <= 0) {
+      return 0;
+    }
+    newCaseNumber = previousCaseNumber - 1;
+  }
+  caseNumberField.value = newCaseNumber;
+  return newCaseNumber;
+}
+
+/* Common function to update total case price according to plus and minus */
+
+function updateCaseTotalPrice(newCaseNumber) {
+  const caseTotalElement = document.getElementById("case-total");
+  const caseTotalPrice = newCaseNumber * 59;
+  caseTotalElement.innerText = caseTotalPrice;
+}
+
+document.getElementById("btn-case-plus").addEventListener("click", function () {
+  const newCaseNumber = updateCaseNumber(true);
+  updateCaseTotalPrice(newCaseNumber);
+});
+
+document
+  .getElementById("btn-case-minus")
+  .addEventListener("click", function () {
+    const newCaseNumber = updateCaseNumber(false);
+    updateCaseTotalPrice(newCaseNumber);
+  });
---
+++ js/phone.js
@@ -0,0 +1,40 @@
+/* Common function to update  phone price according to plus and minus */
+
+function updatePhoneNumber(isIncrease) {
+  const phoneNumberField = document.getElementById("phone-number-field");
+  const phoneNumberString = phoneNumberField.value;
+  const previousPhoneNumber = parseInt(phoneNumberString);
+  let newPhoneNumber;
+
+  if (isIncrease === true) {
+    newPhoneNumber = previousPhoneNumber + 1;
+  } else {
+    if (previousPhoneNumber <= 0) {
+      return 0;
+    }
+    newPhoneNumber = previousPhoneNumber - 1;
+  }
+  phoneNumberField.value = newPhoneNumber;
+  return newPhoneNumber;
+}
+/* Common function to update total phone price according to plus and minus */
+
+function updatePhoneTotalPrice(newPhoneNumber) {
+  const phoneTotalElement = document.getElementById("phone-total");
+  const phoneTotalPrice = newPhoneNumber * 1219;
+  phoneTotalElement.innerText = phoneTotalPrice;
+}
+
+document
+  .getElementById("btn-phone-plus")
+  .addEventListener("click", function () {
+    const newPhoneNumber = updatePhoneNumber(true);
+    updatePhoneTotalPrice(newPhoneNumber);
+  });
+
+document
+  .getElementById("btn-phone-minus")
+  .addEventListener("click", function () {
+    const newPhoneNumber = updatePhoneNumber(false);
+    updatePhoneTotalPrice(newPhoneNumber);
+  });
